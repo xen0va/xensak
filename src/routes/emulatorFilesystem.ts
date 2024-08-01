ac5edb7849034ddd47175a379534cce30e12e3e1
Novα <59028040+xen0va@users.noreply.github.com>
2024-08-01 13:49:47 -0300

    Electron app initial create

---
+++ src/routes/emulatorFilesystem.ts
@@ -0,0 +1,19 @@
+import fs from "fs-extra"
+import path from "path"
+import { app } from "electron"
+import { EmulatorGames } from "src/types"
+
+export const getRyujinxPath = () => {
+    if (process.platform == "win32") {
+        return path.resolve(app.getPath("appData"), "Ryujinx")
+    }
+    else {
+        return path.resolve(app.getPath("home"), ".config", "Ryujinx")
+    }
+}
+
+export const loadGameList = async ():Promise<EmulatorGames> => {
+    const dataPath:string = getRyujinxPath();
+    const directories = fs.readdirSync(path.join(dataPath, "games"), {withFileTypes: true})
+    return directories.filter(d => d.isDirectory()).map(d => d.name.toLowerCase());
+}
