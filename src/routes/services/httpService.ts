d3182d6f1f149b9f01554996e029db74f99f5fe4
Novα <59028040+xen0va@users.noreply.github.com>
2024-08-02 14:15:43 -0300

    implement shader install function

---
+++ src/routes/services/httpService.ts
@@ -0,0 +1,7 @@
+import { URL } from "url"
+
+const CDN_URL:string = "https://localhost:8080/" 
+
+export enum HTTP_PATHS {
+    SHADER_ZIP  = "/nintendo/shaders/{title_id}.zip",
+}
---
+++ src/routes/shaders.ts
@@ -0,0 +1,27 @@
+import http from "http"
+import fs from "fs"
+import path from "path"
+import { getRyujinxPath } from "./emulatorFilesystem"
+
+export const installShaders = async (titleId:string): Promise<boolean> => {
+    const zipUrl = 'http://localhost:8080/' + titleId + '.zip'
+    const shaderDir = path.resolve(getRyujinxPath(), 'games', titleId, 'cache', 'shader')
+    
+    console.log(shaderDir)
+
+    const shaderZip = fs.createWriteStream(path.resolve(shaderDir, `${titleId}.zip` ))
+
+    http.get(zipUrl, (response) => {
+        response.pipe(shaderZip);
+        shaderZip.on('finish', () => {
+            shaderZip.close(() => {
+                console.log(`shader cache for ${titleId} downloaded`)
+                return true
+            });
+        });
+    }).on('error', (err) => fs.unlink(shaderDir, () => {
+        console.log("Download error")
+        return false
+    }))
+    return
+}
